fb6d8a955b708a26d10197f1247da0cde119632b
abner-rivas <90056787+abner-rivas@users.noreply.github.com>
2023-11-26 21:16:07 -0600

    si

--- resources/views/postulados/postulados_consulta.blade.php
+++ resources/views/postulados/postulados_consulta.blade.php
@@ -6,28 +6,28 @@
     </x-slot>
 
     <div id="app" class="p-6">
-        <table class="min-w-full divide-y divide-gray-200">
-            <caption class="text-lg font-semibold mb-4">Lista de Postulados</caption>
-            <thead class="bg-gray-100">
-                <tr>
-                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Usuario</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Texto</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tipo</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fecha de creación</th>
-                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Acciones</th>
+        <table class="table w-full max-h-screen rounded-md">
+            <caption class="text-lg font-semibold text-white mb-4">Lista de Postulados</caption>
+            <thead class="border-b bg-slate-100 dark:bg-gray-700">
+                <tr class="text-center">
+                    <th class="px-6 py-4 text-xs text-gray-500 font-semibold">ID</th>
+                    <th class="px-6 py-4 text-xs text-gray-500 font-semibold">Usuario</th>
+                    <th class="px-6 py-4 text-xs text-gray-500 font-semibold">Texto</th>
+                    <th class="px-6 py-4 text-xs text-gray-500 font-semibold">Tipo</th>
+                    <th class="px-6 py-4 text-xs text-gray-500 font-semibold">Fecha de creación</th>
+                    <th class="px-6 py-4 text-xs text-gray-500 font-semibold">Acciones</th>
                 </tr>
             </thead>
-            <tbody class="bg-white divide-y divide-gray-200">
+            <tbody>
                 @if(isset($catFacts) && is_array($catFacts))
                     @foreach($catFacts as $fact)
-                        <tr>
-                            <td class="px-6 py-4 whitespace-nowrap">{{ $fact['_id'] }}</td>
-                            <td class="px-6 py-4 whitespace-nowrap">{{ $fact['user'] }}</td>
-                            <td class="px-6 py-4 whitespace-nowrap">{{ $fact['text'] }}</td>
-                            <td class="px-6 py-4 whitespace-nowrap">{{ $fact['type'] }}</td>
-                            <td class="px-6 py-4 whitespace-nowrap">{{ $fact['createdAt'] }}</td>
-                            <td class="px-6 py-4 whitespace-nowrap">
+                        <tr class="border-b">
+                            <td class="text-center p-1 text-white">{{ $fact['_id'] }}</td>
+                            <td class="text-center p-1 text-white">{{ $fact['user'] }}</td>
+                            <td class="text-center p-1 text-white">{{ $fact['text'] }}</td>
+                            <td class="text-center p-1 text-white">{{ $fact['type'] }}</td>
+                            <td class="text-center p-1 text-white">{{ $fact['createdAt'] }}</td>
+                            <td class="text-center p-1 text-white">
                                 <a href="{{ route('postulados.revisar', $fact['_id']) }}">
                                     <button type="button" class="text-black bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Revisar</button>
                                 </a>
