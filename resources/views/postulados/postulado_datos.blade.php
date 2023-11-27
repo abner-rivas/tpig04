db397ca1f9ea37b7e40fb8028b5e6dd1ba3b9ab8
kyller7120 <riveramarlon113@gmail.com>
2023-11-26 22:17:00 -0600

    a

--- resources/views/postulados/postulado_datos.blade.php
+++ resources/views/postulados/postulado_datos.blade.php
@@ -32,6 +32,47 @@
                                 </label>
                                 <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="texto" type="text" value="{{ $detalle['text'] }}" readonly>
                             </div>
+
+                            <div class="mb-4">
+                                <label class="block text-gray-700 text-sm font-bold mb-2" for="estado">
+                                    Estado
+                                </label>
+                                @if($detalle['__v'] == 0)
+                                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="estado" type="text" value="no aprobado" readonly>
+                                @elseif($detalle['__v'] == 1)
+                                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="estado" type="text" value="aprobado" readonly>
+                                @endif
+                            </div>
+
+                            <div class="mb-4">
+                                <label class="block text-gray-700 text-sm font-bold mb-2" for="source">
+                                    Source
+                                </label>
+                                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="source" type="text" value="{{ $detalle['source'] }}" readonly>
+                            </div>
+
+                            <div class="mb-4">
+                                <label class="block text-gray-700 text-sm font-bold mb-2" for="type">
+                                    Type
+                                </label>
+                                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="type" type="text" value="{{ $detalle['type'] }}" readonly>
+                            </div>
+                            <div >
+                                <div class="inline-block">
+                                    <td class="text-center p-1 text-white">
+                                        <a href="#">
+                                            <button type="button" class="text-black bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Aprobar</button>
+                                        </a>
+                                    </td>
+                                </div>
+                                <div class="inline-block">
+                                    <td class="text-center p-1 text-white">
+                                        <a href="#">
+                                            <button type="button" class="text-black bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Denegar</button>
+                                        </a>
+                                    </td>
+                                </div>
+                            </div>
                         </form>
                     @else
                         <p>No se encontraron detalles para mostrar.</p>
